04a3224c2eeee3739d872e14601d15de3c2c0b3b
boufiis <bufiyassin@gmail.com>
2025-09-20 22:49:09 +0100

    :tada: initial project

---
+++ AlloJari/AlloJari/AlloJariApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlloJariApp.swift
+//  AlloJari
+//
+//  Created by MAC on 20/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct AlloJariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlloJari/AlloJari/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlloJari
+//
+//  Created by MAC on 20/9/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
